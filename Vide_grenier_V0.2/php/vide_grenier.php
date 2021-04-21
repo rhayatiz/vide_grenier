e70abacdcff61cf7e7a143fa1cf4a2e90d8d2614
Zakaria <rhayatizakaria@gmail.com>
2021-04-21 16:20:58 +0200

    [page vide-grenier] Accordion bootstrap

--- Vide_grenier_V0.2/php/vide_grenier.php
+++ Vide_grenier_V0.2/php/vide_grenier.php
@@ -22,109 +22,6 @@ session_start();
 
 <div class="container">
     <h1 class="display-4 mb-4">Les prochains vides greniers</h1>
-
-    <div class="accordion" id="accordionExample">
-    <div class="accordion-item">
-        <h2 class="accordion-header border-5 border-left border-info" id="headingOne">
-        <div class="accordion-button pl-0 pr-3 py-0" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-            <div class="row justify-content-between p-3" style="width : 100%;">
-                <div class="col-3 pl-4">
-                    <div class="row align-center h4 mb-0">22 SEP</div> 
-                    <div class="row align-center h3">2016</div>
-                </div>
-                <div class="col-7">
-                    <div class="row align-center h4 mb-0">Vide-grenier annuel 2016</div>
-                    <div class="row align-center">Sainte-foy-lès-lyon 69110</div>
-                </div>
-            </div>    
-        </div>
-        </h2>
-        <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
-        <div class="accordion-body" style="background-color: #f7f7f7;">
-            <!-- ACCORDION BODY -->
-            <div class="card-body">
-                  <div class="row">
-                    <div class="col-sm-3 mb-1">
-                      <h6 class="mb-0">Quand</h6>
-                    </div>
-                    <div class="col-sm-9">
-                        <span class="px-sm-3">Le <strong>{{DATE}}</strong> à <strong>{{HEURE}}</strong></span>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3 mb-1">
-                      <h6 class="mb-0">Adresse</h6>
-                    </div>
-                    <div class="col-sm-9">
-                        <span class="px-sm-3">{{Full address}}</span>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3 mb-1">
-                      <h6 class="mb-0">Nombre emplacements</h6>
-                    </div>
-                    <div class="col-sm-9">
-                      <span class="px-sm-3">{{NB_Emplacements}}</span>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col-sm-3 mb-1">
-                      <h6 class="mb-0">Places disponsibles</h6>
-                    </div>
-                    <div class="col-sm-9">
-                      <span style="background-color:#C7E0AF; border-radius: 15px; color:#0D6F1C;" class="py-1 px-3"><strong>{{Places dispo}}</strong> Places disponibles</span>
-                    </div>
-                  </div>
-                  <hr>
-                  <div class="row">
-                    <div class="col text-secondary">
-                        <p class="mb-0">Exposants ou Visiteurs, nous vous attendons nombreux !</p>
-                        <p class="mb-0">Réservé exclusivement aux particuliers.</p>
-                    </div>
-                  </div>
-                  <div class="row justify-content-center mb-2 mt-3">
-                  <div class="col">
-                    <div class="btn mx-auto btn-primary">
-                        Réservez
-                    </div>
-                  </div>
-                  </div>
-                </div>
-              </div>
-            <!-- END ACCORDION BODY -->
-        </div>
-        </div>
-    </div>
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="headingTwo">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-            Accordion Item #2
-        </button>
-        </h2>
-        <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-            <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-        </div>
-    </div>
-    <div class="accordion-item">
-        <h2 class="accordion-header" id="headingThree">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-            Accordion Item #3
-        </button>
-        </h2>
-        <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
-        <div class="accordion-body">
-            <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-        </div>
-        </div>
-    </div>
-    </div>
-</div>
-
     <?php
     try {
         include 'inc_bdd.php';
@@ -135,6 +32,7 @@ session_start();
         $resultat->execute();
 
         echo "<main id=\"videGrenier\" class=\"text-center\">";
+        echo "<div class='accordion mb-4' id='accordionContainer'>";
         if($resultat->rowCount() == 0){
             echo "<div class='mb-4'>AUCUN VIDE GRENIER PROGRAMME</div>";
         }else{
@@ -146,36 +44,112 @@ session_start();
                 $heure = $ligne['HEURE_VG'];
                 $addresse = $ligne['ADDRESSE_VG'];
                 $nbrRestant = $ligne['NBR_RESTANT_VG'];
+                $nbrEmplacements = $ligne['NBR_EMPLACEMENTS'];
+                
         
                 ?>
-                <section class="boxSite">
-                <h3>Prochain événement du CIL de la Gravière:</h3>
-                <h3><?php echo $label ?></h3>
-                <p>Quand? le <?php echo $date ?>, <?php echo $heure ?></p>
-                <p>Où? <?php echo $addresse ?></p>
-                <p>Exposants ou Visiteurs, nous vous attendons nombreux !</p>
-                <p>Réservé exclusivement aux particuliers.</p>
-                <!-- <img src="../images/imgVG.jpg" alt="Image de Vide-grenier" id="imgVG"> -->
+                <!-- Accordion item -->
+                <div class="accordion-item">
+                        <h2 class="accordion-header border-5 border-left border-info" id="<?php echo 'heading'.$id; ?>">
+                        <div class="accordion-button pl-0 pr-3 py-0 collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#<?php echo 'collapse'.$id; ?>" aria-expanded="false" aria-controls="<?php echo 'collapse'.$id; ?>">
+                            <div class="row justify-content-between p-3" style="width : 100%;">
+                                <div class="col-3 pl-4">
+                                    <div class="row align-center h4 mb-0"><?php echo $date ?></div> 
+                                    <div class="row align-center h3"><?php echo $heure ?></div>
+                                </div>
+                                <div class="col-7">
+                                    <div class="row align-center h4 mb-0"><?php echo $label ?></div>
+                                    <div class="row align-center"><?php echo $addresse ?></div>
+                                </div>
+                            </div>    
+                        </div>
+                        </h2>
+                        <div id="<?php echo 'collapse'.$id; ?>" class="accordion-collapse collapse" aria-labelledby="<?php echo 'heading'.$id; ?>" data-bs-parent="#accordionContainer">
+                        <div class="accordion-body" style="background-color: #f7f7f7;">
+                            <!-- ACCORDION BODY -->
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-3 mb-1">
+                                    <h6 class="mb-0">Quand</h6>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="px-sm-3">Le <strong><?php echo $date ?></strong> à <strong><?php echo $heure ?></strong></span>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3 mb-1">
+                                    <h6 class="mb-0">Adresse</h6>
+                                    </div>
+                                    <div class="col-sm-9">
+                                        <span class="px-sm-3"><?php echo $addresse ?></span>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3 mb-1">
+                                    <h6 class="mb-0">Nombre emplacements</h6>
+                                    </div>
+                                    <div class="col-sm-9">
+                                    <span class="px-sm-3"><?php echo $nbrEmplacements ?></span>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col-sm-3 mb-1">
+                                    <h6 class="mb-0">Places disponsibles</h6>
+                                    </div>
+                                    <div class="col-sm-9">
+                                    <!-- PLACES DISPO -->
+                                    <?php 
+                                    if ($nbrRestant > 0) {
+                                        
+                                        // NOMBRE DE PLACES
+                                        echo '<span style="background-color:#C7E0AF; border-radius: 15px; color:#0D6F1C;" class="py-1 px-3"><strong>'.$nbrRestant.'</strong> Places disponibles</span>';
+
+                                        // USER connecté, bouton réservez
+                                        if (isset($_SESSION['id_util'])) {
+                            
+                                            echo '<div class="row justify-content-center mb-2 mt-3">
+                                                    <div class="col">
+                                                            <a class="btn btn-primary"
+                                                            href="reservation.php?idVG=' . $id . '">Réservez!</a>
+                                                    </div>
+                                                </div>';
+                                        // User deconnecté, bouton connexion
+                                        } else {
+                                            echo '<div class="row justify-content-center mb-2 mt-3">
+                                                <div class="col">
+                                                    <a class="btn btn-primary"
+                                                    href="connexion.php">Connectez vous pour réserver</a>
+                                                </div>
+                                            </div>';
+                                        }
+                                    } else {
+                                        // PLUS DE PLACES DISPO
+                                        echo '<span style="background-color:#EDA7A7; border-radius: 15px; color:#6E0000;" class="py-1 px-3">Plus de places disponibles</span>';
+                                    }
+                                    ?>
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="row">
+                                    <div class="col text-secondary">
+                                        <p class="mb-0">Exposants ou Visiteurs, nous vous attendons nombreux !</p>
+                                        <p class="mb-0">Réservé exclusivement aux particuliers.</p>
+                                    </div>
+                                </div>
+
+                                </div>
+                            </div>
+                            <!-- END ACCORDION BODY -->
+                        </div>
+                    </div>
+                    <!-- END ITEM -->
                 <?php
         
-                if ($nbrRestant > 0) {
-        
-                    echo "<p>Plus que $nbrRestant places disponibles!</p>";
-        
-                    if (isset($_SESSION['id_util'])) {
-        
-                        echo "<a href=\"reservation.php?idVG=" . $id . "\" class=\"bouton\">Réservez!</a>";
-                    } else {
-            
-                        echo "<a href=\"connexion.php\" class=\"bouton\">Connectez vous pour réserver</a>";
-                    }
-                } else {
-        
-                    echo "<p>Plus de places disponibles!</p>";
-                }
-        
-                echo "</section>";
 
+        
             }
         }
 
@@ -187,10 +161,10 @@ session_start();
     }
 
     ?>
-
-    
-        
+        </div>
     </main>
+</div>
+
 
 
 
